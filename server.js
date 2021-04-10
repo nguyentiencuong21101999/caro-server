8fb7b0227a86fc2ef1f791c675910470483d9301
Your Name <nguyentiencuong21101999>
2021-04-10 15:10:13 +0700

    fix-server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
          origin: "https://messengerss.herokuapp.com",
-        origin: "http://localhost:3000",
+       // origin: "http://localhost:3000",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
